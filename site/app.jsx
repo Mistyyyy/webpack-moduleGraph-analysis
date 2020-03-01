e317f8c6dffb10de6d7a1a3f3c67ade962ed5aeb
dingchunlei <harley.mistyyyy@gmail.com>
2020-03-01 11:32:07 +0800

    feat(client websocket): add websocket in client to fetch graph data

--- site/app.jsx
+++ site/app.jsx
@@ -8,7 +8,22 @@ import "./root.css";
 
 const { TabPane } = Tabs;
 
+let ws;
+
+ws = new WebSocket(`ws://${location.host}`);
+
+ws.onopen = function(){
+  ws.send('fetch Data')
+}
+
+ws.onmessage = function(e, data){
+  console.log(e.data.text().then(console.log))
+}
+
+const rootElement = document.getElementById("root");
+
 const App = () => {
+
   const data = Utils.mock(13).circle().graphin();
 
   return (
@@ -25,6 +40,13 @@ const App = () => {
   );
 };
 
-const rootElement = document.getElementById("root");
+// window.addEventListener('load', () => {
 
-ReactDOM.render(<App />, rootElement);
+  ReactDOM.render(<App />, rootElement)
+
+//   if (ws) {
+//     ws.addEventListener('message', event => {
+//       console.log(event)
+//     });
+//   }
+// }, false);
